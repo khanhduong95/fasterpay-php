97acf99028535902e31ad6bad73ab64244c53a0e
James <james@paymentwall.com>
2025-08-11 11:42:04 +0700

    support hash_equals for PHP 5.4

--- lib/autoload.php
+++ lib/autoload.php
@@ -7,6 +7,23 @@ if (!function_exists('json_decode')) {
     throw new Exception('JSON PHP extension is required');
 }
 
+if (!function_exists('hash_equals')) {
+    function hash_equals($a, $b) {
+        echo 'strlen($a): ' . strlen($a);
+        echo 'strlen($b): ' . strlen($b);
+        if (strlen($a) !== strlen($b)) {
+            return false;
+        }
+        
+        $result = 0;
+        for ($i = 0; $i < strlen($a); $i++) {
+            $result |= ord($a[$i]) ^ ord($b[$i]);
+        }
+        echo $result === 0 ? 'YYEESSS' : 'NONONON';
+        return $result === 0;
+    }
+}
+
 function autoload($className)
 {
     if (strpos($className, 'FasterPay') !== 0) {
